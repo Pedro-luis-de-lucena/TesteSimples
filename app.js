37b55334a4cdf412d08adf36f92e33ff6ce7589e
pedro castro <evafle@hotmail.com>
2022-08-02 08:56:21 -0300

    Update app.js
    Ola, fiz de duas formas, as duas fazem mas so uma passa no test

--- app.js
+++ app.js
@@ -35,4 +35,21 @@ function macsByRadio(probes){
     return [radio1, radio2, radio3, radio4]
 }
 
+ ### esse abaixo e mais bonito de ser feito mas nao consegui passar no test( mas faz exatamente o que se pede).
+// const groupBy = (array, key) => {
+//     // Return the end result
+//     return array.reduce((result, currentValue) => {
+//       // If an array already present for key, push it to the array. Else create an array and push the object
+//       (result[currentValue[key]] = result[currentValue[key]] || []).push(
+//         currentValue
+//       );
+//       // Return the current iteration `result` value, this will be taken as next iteration `result` value and accumulate
+//       return result;
+//     }, {}); // empty object is the initial value for result object
+//   };
+  
+//   // Group by color as key to the person array
+//   const group = groupBy(probes, 'radio');
+//   console.log(group)
+
 module.exports = { macsByRadio };
